0068fe557988e1a0955d3a05d527d1d0a410c290
Sathisks23 <sathishksdckap@gmail.com>
2025-02-21 14:42:38 +0530

    checking env

--- src/lib/Connection.js
+++ src/lib/Connection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export async function Connect(){
     try {
         mongoose.connect(process.env.MONGODB_URI)
-        console.log('Mongodb Connected...')
+        console.log('Mongodb Connected...',process.env.MONGODB_URI)
     } catch (error) {
         console.log(error.message);
     }
