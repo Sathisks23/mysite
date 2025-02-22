f48ab8f7ec4b452c19e75cfe7ecc5016891b90fa
Sathisks23 <sathishksdckap@gmail.com>
2025-02-22 12:47:54 +0530

    google search console add

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,6 +29,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <meta name="google-site-verification" content="uvZ3FIqlGxeTrYfKvjDBMVI980FIDsfUvG3Lj0lCvls" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased pt-20`}
       >
